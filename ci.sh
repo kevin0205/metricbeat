56c455751db33da06101528231dd2e0b5c11d5a3
kevin0205 <52814458+kevin0205@users.noreply.github.com>
2020-06-26 01:31:39 +0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -3,7 +3,8 @@
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   docker buildx build \
     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+    --platform=linux/amd64 \
+    --build-arg VERSION=7.5.2 \
     .
   exit $?
 fi
@@ -11,7 +12,8 @@ echo $DOCKER_PASSWORD | docker login -u qmcgaw --password-stdin &> /dev/null
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
      --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+    --platform=linux/amd64 \
+    --build-arg VERSION=7.5.2 \
     -t $DOCKER_REPO:$TAG \
     --push \
     .
