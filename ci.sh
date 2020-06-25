2cb13095f8aeecbdb790e7cb835afa840b8279ca
kevin0205 <52814458+kevin0205@users.noreply.github.com>
2020-06-26 00:23:30 +0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -11,8 +11,8 @@ fi
 echo $DOCKER_PASSWORD | docker login -u kevin0205 --password-stdin &> /dev/null
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
-    --progress plain \
-    --platform=linux/amd64,linux/arm64 \
+     --progress plain \
+    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
     --build-arg VERSION=7.5.2 \
     -t $DOCKER_REPO:$TAG \
     --push \
