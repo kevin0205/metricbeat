f6fcdb48956b587546664517ca677d39b6872490
kevin0205 <52814458+kevin0205@users.noreply.github.com>
2020-06-28 22:31:22 +0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -15,6 +15,6 @@ docker buildx build \
     --platform=$TARGET_ARCH \
     --build-arg VERSION=$TARGET_VERSION \
     -t $DOCKER_REPO \
-    -t $DOCKER_REPO:$TAG \
+    -t $DOCKER_REPO:$TARGET_VERSION \
     --push \
     .
