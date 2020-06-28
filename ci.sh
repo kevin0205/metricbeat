07f4f846cee811eb2ce7eb8901a840f126046e79
kevin0205 <52814458+kevin0205@users.noreply.github.com>
2020-06-28 23:08:05 +0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
-  docker buildx build \
-    --progress plain \
-    --platform=$TARGET_ARCH \
-    --build-arg VERSION=$TARGET_VERSION \
-    .
-  exit $?
-fi
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin &> /dev/null
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
